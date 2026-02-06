530bf4f1435dc01e14b05a8b97d32f7857d6d137
likki <likhithareddy753@gmail.com>
2026-02-06 16:33:53 +0530

    completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
-import ThemeProvider from './context/ThemeProvider'
+import ThemeProvider from './context/Themeprovider'
 import AuthProvider from './context/AuthProvider'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
